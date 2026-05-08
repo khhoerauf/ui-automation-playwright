82d0cd20dc1a28db47b8ec37a4ffb311c6389ae9
kingahoerauf <kingahoerauf@gmail.com>
2026-05-08 15:02:50 -0600

    ignore some folder, format file

--- tests/spec/videoModel.spec.js
+++ tests/spec/videoModel.spec.js
@@ -1,7 +1,8 @@
-import { test } from '@playwright/test';
-import { VideoModel } from '../pages/videoModel.js';
+import { test } from "@playwright/test";
+import { VideoModel } from "../pages/videoModel.js";
 
-test('video should start after click start button', async ({ page }) => {
+test.describe('feature: video', () => {
+  test("video should start after click start button", async ({ page }) => {
     const video = new VideoModel(page);
     await video.openVideoFile();
 
@@ -9,18 +10,18 @@ test('video should start after click start button', async ({ page }) => {
     await video.clickStartButton();
     await video.isLandingCardVisible(false);
     await video.isVideoPlaying();
-});
+  });
 
-test('video should auto start with 2 seconds delay', async ({ page }) => {
+  test("video should auto start with 2 seconds delay", async ({ page }) => {
     const video = new VideoModel(page);
     await video.openVideoFile();
 
     await video.isLandingCardVisible();
     await video.isLandingCardVisible(false);
     await video.isVideoPlaying();
-});
+  });
 
-test('video should be able to skip after 5 seconds', async ({ page }) => {
+  test("video should be able to skip after 5 seconds", async ({ page }) => {
     const video = new VideoModel(page);
     await video.openVideoFile();
 
@@ -30,9 +31,9 @@ test('video should be able to skip after 5 seconds', async ({ page }) => {
     await video.isVideoPlaying();
     await video.clickSkipButton();
     await video.isEndCardVisible();
-});
+  });
 
-test('close button should hide end card', async ({ page }) => {
+  test("close button should hide end card", async ({ page }) => {
     const video = new VideoModel(page);
     await video.openVideoFile();
 
@@ -42,4 +43,5 @@ test('close button should hide end card', async ({ page }) => {
     await video.isEndCardVisible();
     await video.clickCloseButton();
     await video.isEndCardVisible(false);
+  });
 });
